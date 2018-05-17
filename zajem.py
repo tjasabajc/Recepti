0fc88dec99907694ddf61df352918c1820d786fd
Peter Jereb <peter.jereb@student.fmf.uni-lj.si>
2018-05-17 13:34:34 +0200

    Več podatkov. Podatke je treba še združiti in nekatere malo obdelati.

--- zajem.py
+++ zajem.py
@@ -1,6 +1,6 @@
 import re
 import orodja
-import requests
+#import requests
 
 def zajemi():
     for id in range(1, 401):
@@ -12,20 +12,32 @@ def zajemi():
 
 def pocisti(recept):
     podatki = recept.groupdict()
-    podatki['ime_recepta'] = str(podatki['ime_recepta'])
-    podatki['ime_uporabnika'] = str(podatki['ime_uporabnika'])
-    podatki['leto'] = int(podatki['leto'])
-    podatki['mesec'] = int(podatki['mesec'])
-    podatki['dan'] = int(podatki['dan'])
-    podatki['cas_priprave'] = str(podatki['cas_priprave'])
+##    podatki['ime_recepta'] = str(podatki['ime_recepta'])
+##    podatki['ime_uporabnika'] = str(podatki['ime_uporabnika'])
+##    podatki['leto'] = int(podatki['leto'])
+##    podatki['mesec'] = int(podatki['mesec'])
+##    podatki['dan'] = int(podatki['dan'])
+##    podatki['cas_priprave'] = str(podatki['cas_priprave'])
+##    podatki['ocena'] = int(podatki['ocena'])
+##    podatki['stevilo_mnenj'] = int(podatki['stevilo_mnenj'])
+##    podatki['navodilo'] = str(podatki['navodilo'])
+##    podatki['vrsta_jedi'] = str(podatki['vrsta_jedi'])
+    podatki['sestavina'] = str(podatki['sestavina'])
+##    podatki['priloznost'] = str(podatki['priloznost'])
+    
     return podatki
 
 regex_recepta = re.compile(
-    #r'<title>Recept: (?P<ime_recepta>(.*?))</title>.*?'
-    r"itemprop='author'>(?P<ime_uporabnika>(.*?))<.*?"
-    r"itemprop='datePublished'>(?P<dan>(\d{1,2})).(?P<mesec>(\d{1,2})).(?P<leto>(\d{4}))</span.*?"
-    #r'''itemprop='datePublished'>(?P<dan>(\d{1,2})).(?P<mesec>(\d{1,2})).(?P<leto>(\d{4}))</span.*?'''
-        #r'''<span class='cas'>(?P<cas_priprave>)'''
+##    r'<title>Recept: (?P<ime_recepta>(.*?))</title>.*?'
+##    r"itemprop='author'>(?P<ime_uporabnika>(.*?))<.*?"
+##    r"itemprop='datePublished'>(\d{1,2}).(\d{1,2}).(\d{4})</span.*?"
+##    r"itemprop='datePublished'>(?P<dan>(\d{1,2})).(?P<mesec>(\d{1,2})).(?P<leto>(\d{4}))</span.*?"
+##    r"</span><span class='after1'>priložnost: (?P<priloznost>(.*?))</span>.*?"
+##    r"<span class='cas'>(?P<cas_priprave>(.*?))</span>.*?"
+##    r"<span class='tiptip' title='1 ocena'> povprečna ocena: <a href='/baze/popup-ocene.asp?ID=1' id='popup-oceneseznam-holder'><span itemprop='ratingvalue'>(?P<ocena>(\d{1}))</span></a></span><span itemprop='reviewcount'>(?P<stevilo_mnenj>(\d{1}))</span>.*?"
+##    r'''<p class="cf" itemprop="recipeInstructions"><span class="label"></span><span class="fullwidth data">(?P<navodilo>(.*?))</span></p>.*?'''
+##    r"<span itemprop='recipeCategory' style='display:none'>(?P<vrsta_jedi>(.*?))</span>.*?"
+    r'''<p class="cf" itemprop="recipeIngredient"><span class="label">.*?</span><span class="label-value">(?P<sestavina>(.*?))</span>'''
         , flags=re.DOTALL
         )
 
@@ -33,9 +45,9 @@ regex_recepta = re.compile(
 def izloci_podatke(imenik):
     recepti = []
     for html_datoteka in orodja.datoteke(imenik):
-        print(orodja.vsebina_datoteke(html_datoteka))
+        #print(orodja.vsebina_datoteke(html_datoteka))
         for recept in re.finditer(regex_recepta, orodja.vsebina_datoteke(html_datoteka)):
-            print('     *')
+            #print('     *')
             recepti.append(pocisti(recept))
     return recepti
 
@@ -43,8 +55,8 @@ def izloci_podatke(imenik):
 # zajemi()
 
 
-#recepti = izloci_podatke('Recepti/')
-#orodja.zapisi_tabelo(recepti, ['ime_uporabnika', 'leto', 'mesec'], 'test.csv')
+recepti = izloci_podatke('Recepti/')
+orodja.zapisi_tabelo(recepti, ['sestavina'], 'sestavina.csv')
 
 vsebina1 = orodja.vsebina_datoteke('Recepti/001.html')
 
